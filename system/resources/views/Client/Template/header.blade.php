0cf35795438c2930e15a32fa4a16dbe293efde0d
Noberto <noberto.1411@gmail.com>
2020-11-30 04:35:22 +0700

    Initial commit

---
+++ system/resources/views/Client/Template/header.blade.php
@@ -0,0 +1,13 @@
+<header class="masthead" style="background-image: url('{{url('public')}}/client/img/Sepatu.jpg')">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="site-heading">
+            <h1>MERAMPUT-BLOG</h1>
+            <span class="subheading"> Sebuah ARTIKEL tentang Sepatu Untuk style milenial masa kini </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
